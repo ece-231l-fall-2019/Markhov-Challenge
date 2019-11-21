1a4804c6ff93cc69805811f41874bb8746a4bae0
hquang1999 <hquang@unm.edu>
2019-11-21 16:18:20 -0700

    init commit

--- runtests.cpp
+++ runtests.cpp
@@ -5,35 +5,51 @@
 int main ()
 {	
 	size_t steps;
-	int test[][] = {0};	
 	
-	int test[0][0] = 50;
-	int test[0][1] = 50;
+	int test[10][10];	
+	
+	test[0][0] = 50;
+	test[0][1] = 50;
 
-	int test[1][0] = 50;
-	int test[1][3] = 50;
+	test[1][0] = 50;
+	test[1][3] = 50;
 	
-	int test[2][1] = 50;
-	int test[2][4] = 50;
+	test[2][1] = 50;
+	test[2][4] = 50;
 	
-	int test[3][2] = 50;
-	int test[3][5] = 50;
+	test[3][2] = 50;
+	test[3][5] = 50;
 	
-	int test[4][3] = 50;
-	int test[4][6] = 50;
+	test[4][3] = 50;
+	test[4][6] = 50;
 	
-	int test[5][4] = 50;
-	int test[5][7] = 50;
+	test[5][4] = 50;
+	test[5][7] = 50;
 	
-	int test[6][5] = 50;
-	int test[6][8] = 50;
+	test[6][5] = 50;
+	test[6][8] = 50;
 	
-	int test[7][6] = 50;
-	int test[7][8] = 50;
+	test[7][6] = 50;
+	test[7][8] = 50;
 	
-	int test[8][7] = 50;
-	int test[8][9] = 50;
+	test[8][7] = 50;
+	test[8][9] = 50;
 	
+	int node = 0;
+	int dice = 0;
+
+	while (node != 9)
+	{
+		tPerc = 0;
+		dice = rand() % 100;
+		for (int n = 0; n < 10; n++)
+		{
+			tPerc += test[n][node];
+			node = n;
+			steps++;
+			break;
+		}
+	}
 
 	return 0;
 }
