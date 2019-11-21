2745a5b08bfaebdba6a87e35420819c94806b940
hquang1999 <hquang@unm.edu>
2019-11-21 16:00:22 -0700

    update

---
+++ runtests.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main ()
+{	
+	size_t steps;
+	int test[][] = {0};	
+	
+	int test[0][0] = 50;
+	int test[0][1] = 50;
+
+	int test[1][0] = 50;
+	int test[1][3] = 50;
+	
+	int test[2][1] = 50;
+	int test[2][4] = 50;
+	
+	int test[3][2] = 50;
+	int test[3][5] = 50;
+	
+	int test[4][3] = 50;
+	int test[4][6] = 50;
+	
+	int test[5][4] = 50;
+	int test[5][7] = 50;
+	
+	int test[6][5] = 50;
+	int test[6][8] = 50;
+	
+	int test[7][6] = 50;
+	int test[7][8] = 50;
+	
+	int test[8][7] = 50;
+	int test[8][9] = 50;
+	
+
+	return 0;
+}
+
