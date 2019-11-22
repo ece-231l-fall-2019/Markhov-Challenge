64162556cd16cbb28e385e27670b32a4d6435cf7
Nigel Damian <ndamian4@unm.edu>
2019-11-21 17:46:43 -0700

    init

--- runtests.cpp
+++ runtests.cpp
@@ -4,7 +4,7 @@
 int main ()
 {	
 	srand(time(NULL));
-#if 1
+#if 0
 	int row = 9;
 	int column = 10;
 	int test2[row][column] = {
@@ -33,7 +33,7 @@ int main ()
 	        , { 0, 0, 0, 0, 0, 0, 0, 50, 0, 50 }};
 #endif
 
-#if 0
+#if 1
 	int test[10][10] {};	
 
 	test[0][0] = 50;
@@ -63,27 +63,37 @@ int main ()
 	test[8][7] = 50;
 	test[8][9] = 50;
 #endif	
-	int node = 0;
-	int SIM = 9;
-	int Steps = 0;
-	
-	while(node != SIM)
+	int results[SIM] = {0};
+	for(int n; n < SIM; n++)
 	{
-		int TotPercent = 0;
-		int Dice = rand() % 100;
-		for(int next = 0; next < 10; next ++)
+		int node = 0;
+		int SIM = 9;
+		int Steps = 0;
+		while(node != 9)
 		{
-			TotPercent += test[node][next];
-			if(Dice < TotPercent)
+			srand(time(NULL));
+			int TotPercent = 0;
+			int Dice = rand() % 100;
+			for(int next = 0; next < 10; next ++)
 			{
-				Steps ++; 
-				node = next;
-				break;
+				TotPercent += test[node][next];
+				if(Dice < TotPercent)
+				{
+					Steps ++; 
+					node = next;
+					break;
+				}
+				Steps ++;
 			}
-			Steps ++;
 		}
+		results[n] = Steps;
 	}
-	std::cout << Steps << std::endl;	
+
+	for (auto n : results)
+	{
+		std::cout<< n << std::endl;
+	}
+		//std::cout << Steps << std::endl;	
 	return 0;
 }
 
