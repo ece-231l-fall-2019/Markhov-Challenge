ef6c0a182f294d8d96fbaa6739e0c65cf20299f4
hquang1999 <hquang@unm.edu>
2019-11-21 18:29:55 -0700

    Working Prog

--- runtests.cpp
+++ runtests.cpp
@@ -11,22 +11,22 @@ int main ()
 	test[0][1] = 50;
 
 	test[1][0] = 50;
-	test[1][3] = 50;
+	test[1][2] = 50;
 	
 	test[2][1] = 50;
-	test[2][4] = 50;
+	test[2][3] = 50;
 	
 	test[3][2] = 50;
-	test[3][5] = 50;
+	test[3][4] = 50;
 	
 	test[4][3] = 50;
-	test[4][6] = 50;
+	test[4][5] = 50;
 	
 	test[5][4] = 50;
-	test[5][7] = 50;
+	test[5][6] = 50;
 	
 	test[6][5] = 50;
-	test[6][8] = 50;
+	test[6][7] = 50;
 	
 	test[7][6] = 50;
 	test[7][8] = 50;
@@ -34,21 +34,21 @@ int main ()
 	test[8][7] = 50;
 	test[8][9] = 50;
 	
-	test[9][8] = 50;
-	test[9][10] = 50;
+//	test[9][8] = 50;
+//	test[9][10] = 50;
 	
 	int node = 0;
 	int sim = 10000;
 	size_t steps = 0;
 	int results[sim] = {0};
-
+	int Dice = rand() % 100;
+	
 	for (int n = 0; n < sim; n++) 
 	{
 		while(node != 9)
 		{
 			int tPercent = 0;
-			int Dice = rand() % 100;
-		
+			Dice = rand() % 100;
 			for(int next = 0; next < 10; next++)
 			{
 				tPercent += test[node][next];
@@ -58,8 +58,8 @@ int main ()
 					steps++; 
 					break;
 				}
-				steps++;
 			}
+			//steps++;
 		}
 		results[n] = steps;
 		node = 0;
@@ -70,7 +70,7 @@ int main ()
 	int resultsMin = results[0];
 	int mean = 0;
 
-	for (int n = 0; (n + 1) <= sim; n++)
+	for (int n = 0; n < sim; n++)
 	{
 		int ind = results[n];
                 mean += results[n];
@@ -87,8 +87,13 @@ int main ()
 	}
 
 	int resultsMean = (mean/sim);
- 
-        std::cout << "Max: " << resultsMax << std::endl;
+/*	
+	for (int n = 0; n < 50; n++)
+	{	
+		std::cout << results[n] << std::endl;
+	}
+  */
+  	std::cout << "Max: " << resultsMax << std::endl;
         std::cout << "Min: " << resultsMin << std::endl;
         std::cout << "Mean: " << resultsMean << std::endl;
 
